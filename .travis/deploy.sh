78b20f02f362eb11ffe55cb599a540765e08e85e
Hector Castro <hectcastro@gmail.com>
2015-08-29 20:01:30 -0400

    Ensure reference to built image is used as base for tag

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,6 +6,8 @@ if [ -z "${TRAVIS_TAG}" ]; then
   QUAY_TAG="${TRAVIS_COMMIT:0:7}"
 else
   QUAY_TAG="${TRAVIS_TAG}"
+
+  docker tag -f "quay.io/${TRAVIS_REPO_SLUG:0:6}/${TRAVIS_REPO_SLUG:14}:${TRAVIS_COMMIT:0:7}" "quay.io/${TRAVIS_REPO_SLUG:0:6}/${TRAVIS_REPO_SLUG:14}:${QUAY_TAG}"
 fi
 
 docker push "quay.io/${TRAVIS_REPO_SLUG:0:6}/${TRAVIS_REPO_SLUG:14}:${QUAY_TAG}"
